34ddb4dbc4aad1b94fa064d93d3a01db5871fb4f
Antoine <end2endzone@gmail.com>
2018-08-04 16:38:33 -0400

    * Fixed issue #4 - License header in all files * Added LICENSE header to all source and header files. * Created LICENSE.h file which is used to generate version.h and config.h

--- LICENSE.h
+++ LICENSE.h
@@ -21,23 +21,3 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *********************************************************************************/
-
-#ifndef TEST_RA_ENVIRONMENT_H
-#define TEST_RA_ENVIRONMENT_H
-
-#include <gtest/gtest.h>
-
-namespace ra { namespace environment { namespace test
-{
-  class TestEnvironment : public ::testing::Test
-  {
-  public:
-    virtual void SetUp();
-    virtual void TearDown();
-  };
-
-} //namespace test
-} //namespace environment
-} //namespace ra
-
-#endif //TEST_RA_ENVIRONMENT_H
