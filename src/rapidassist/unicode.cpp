95837b76983944a49c966fbc6f35be86d1fb9c27
Antoine <end2endzone@gmail.com>
2020-02-29 10:32:25 -0500

    * Fixed issue #49: Create ra::unicode namespace to wrap functions that provide support to unicode.

---
+++ src/rapidassist/unicode.cpp
@@ -0,0 +1,238 @@
+/**********************************************************************************
+ * MIT License
+ * 
+ * Copyright (c) 2018 Antoine Beauchamp
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *********************************************************************************/
+
+#include "rapidassist/unicode.h"
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+namespace ra { namespace unicode {
+
+  static const std::string  EMPTY_STRING;
+  static const std::wstring EMPTY_WIDE_STRING;
+
+  bool IsAscii(const char * str)
+  {
+    int offset = 0;
+    while (str[offset] != '\0')
+    {
+      if (str[offset] < 0) //if bit7 is set
+        return false;
+
+      //next byte
+      offset++;
+    }
+    return true;
+  }
+
+  bool IsValidCp1252(const char * str)
+  {
+    int offset = 0;
+    while (str[offset] != '\0')
+    {
+      const char & c = str[offset];
+      if (
+        c == 0x81 ||
+        c == 0x8D ||
+        c == 0x8F ||
+        c == 0x90 ||
+        c == 0x9D )
+        return false;
+
+      //next byte
+      offset++;
+    }
+    return true;
+  }
+
+  bool IsValidIso8859_1(const char * str)
+  {
+    int offset = 0;
+    while (str[offset] != '\0')
+    {
+      const char & c = str[offset];
+      if (0x00 <= c && c <= 0x1F)
+        return false;
+      if (0x7F <= c && c <= 0x9F)
+        return false;
+
+      //next byte
+      offset++;
+    }
+    return true;
+  }
+
+  bool IsValidUtf8(const char * str)
+  {
+    int offset = 0;
+    while (str[offset] != '\0')
+    {
+      const char & c1 = str[offset + 0];
+      char c2 = str[offset + 1];
+      char c3 = str[offset + 2];
+      char c4 = str[offset + 3];
+    
+      //prevent going outside of the string
+      if (c1 == '\0')
+        c2 = c3 = c4 = '\0';
+      else if (c2 == '\0')
+        c3 = c4 = '\0';
+      else if (c3 == '\0')
+        c4 = '\0';
+
+      //size in bytes of the code point
+      int n = 1;
+
+      //See http://www.unicode.org/versions/Unicode6.0.0/ch03.pdf, Table 3-7. Well-Formed UTF-8 Byte Sequences
+      // ## | Code Points         | First Byte | Second Byte | Third Byte | Fourth Byte
+      // #1 | U+0000   - U+007F   | 00 - 7F    |             |            | 
+      // #2 | U+0080   - U+07FF   | C2 - DF    | 80 - BF     |            | 
+      // #3 | U+0800   - U+0FFF   | E0         | A0 - BF     | 80 - BF    | 
+      // #4 | U+1000   - U+CFFF   | E1 - EC    | 80 - BF     | 80 - BF    | 
+      // #5 | U+D000   - U+D7FF   | ED         | 80 - 9F     | 80 - BF    | 
+      // #6 | U+E000   - U+FFFF   | EE - EF    | 80 - BF     | 80 - BF    | 
+      // #7 | U+10000  - U+3FFFF  | F0         | 90 - BF     | 80 - BF    | 80 - BF
+      // #8 | U+40000  - U+FFFFF  | F1 - F3    | 80 - BF     | 80 - BF    | 80 - BF
+      // #9 | U+100000 - U+10FFFF | F4         | 80 - 8F     | 80 - BF    | 80 - BF
+
+      if (c1 <= 0x7F) // #1 | U+0000   - U+007F, (ASCII)
+        n = 1;
+      else if ( 0xC2 <= c1 && c1 <= 0xDF &&
+                0x80 <= c2 && c2 <= 0xBF)  // #2 | U+0080   - U+07FF
+        n = 2;
+      else if ( 0xE0 == c1 &&
+                0xA0 <= c2 && c2 <= 0xBF &&
+                0x80 <= c3 && c3 <= 0xBF)  // #3 | U+0800   - U+0FFF
+        n = 3;
+      else if ( 0xE1 <= c1 && c1 <= 0xEC &&
+                0x80 <= c2 && c2 <= 0xBF &&
+                0x80 <= c3 && c3 <= 0xBF)  // #4 | U+1000   - U+CFFF
+        n = 3;
+      else if ( 0xED == c1 &&
+                0x80 <= c2 && c2 <= 0x9F &&
+                0x80 <= c3 && c3 <= 0xBF)  // #5 | U+D000   - U+D7FF
+        n = 3;
+      else if ( 0xEE <= c1 && c1 <= 0xEF &&
+                0x80 <= c2 && c2 <= 0xBF &&
+                0x80 <= c3 && c3 <= 0xBF)  // #6 | U+E000   - U+FFFF
+        n = 3;
+      else if ( 0xF0 == c1 &&
+                0x90 <= c2 && c2 <= 0xBF &&
+                0x80 <= c3 && c3 <= 0xBF &&
+                0x80 <= c4 && c4 <= 0xBF)  // #7 | U+10000  - U+3FFFF
+        n = 4;
+      else if ( 0xF1 <= c1 && c1 <= 0xF3 &&
+                0x80 <= c2 && c2 <= 0xBF &&
+                0x80 <= c3 && c3 <= 0xBF &&
+                0x80 <= c4 && c4 <= 0xBF)  // #8 | U+40000  - U+FFFFF
+        n = 4;
+      else if ( 0xF4 == c1 &&
+                0x80 <= c2 && c2 <= 0xBF &&
+                0x80 <= c3 && c3 <= 0xBF &&
+                0x80 <= c4 && c4 <= 0xBF)  // #7 | U+10000  - U+3FFFF
+        n = 4;
+      else
+        return false; // invalid UTF-8 sequence
+
+      //next code point
+      offset += n;
+    }
+    return true;
+  }
+
+
+
+//--------------------------------------------------------------------------------------------------
+#ifdef _WIN32
+//--------------------------------------------------------------------------------------------------
+
+  // Convert a wide Unicode string to an UTF8 string
+  std::string UnicodeToUtf8(const std::wstring & wstr)
+  {
+    if (wstr.empty()) return std::string();
+    int num_characters = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+    if (num_characters == 0)
+      return EMPTY_STRING;
+    std::string strTo(num_characters, 0);
+    WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], num_characters, NULL, NULL);
+    return strTo;
+  }
+
+  // Convert an UTF8 string to a wide Unicode String
+  std::wstring Utf8ToUnicode(const std::string & str)
+  {
+    if (str.empty()) return std::wstring();
+    int num_characters = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+    if (num_characters == 0)
+      return EMPTY_WIDE_STRING;
+    std::wstring wstrTo(num_characters, 0);
+    MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], num_characters);
+    return wstrTo;
+  }
+
+  // Convert an wide Unicode string to ANSI string
+  std::string UnicodeToAnsi(const std::wstring & wstr)
+  {
+    if (wstr.empty()) return std::string();
+    int num_characters = WideCharToMultiByte(CP_ACP, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+    if (num_characters == 0)
+      return EMPTY_STRING;
+    std::string strTo(num_characters, 0);
+    WideCharToMultiByte(CP_ACP, 0, &wstr[0], (int)wstr.size(), &strTo[0], num_characters, NULL, NULL);
+    return strTo;
+  }
+
+  // Convert an ANSI string to a wide Unicode String
+  std::wstring AnsiToUnicode(const std::string & str)
+  {
+    if (str.empty()) return std::wstring();
+    int num_characters = MultiByteToWideChar(CP_ACP, 0, &str[0], (int)str.size(), NULL, 0);
+    if (num_characters == 0)
+      return EMPTY_WIDE_STRING;
+    std::wstring wstrTo(num_characters, 0);
+    MultiByteToWideChar(CP_ACP, 0, &str[0], (int)str.size(), &wstrTo[0], num_characters);
+    return wstrTo;
+  }
+
+  std::string Utf8ToAnsi(const std::string & str)
+  {
+    std::wstring str_unicode = Utf8ToUnicode(str);
+    std::string str_ansi = UnicodeToAnsi(str_unicode);
+    return str_ansi;
+  }
+
+  std::string AnsiToUtf8(const std::string & str)
+  {
+    std::wstring str_unicode = AnsiToUnicode(str);
+    std::string str_utf8 = UnicodeToUtf8(str_unicode);
+    return str_utf8;
+  }
+
+//--------------------------------------------------------------------------------------------------
+#endif //_WIN32
+//--------------------------------------------------------------------------------------------------
+
+} //namespace unicode
+} //namespace ra
---
+++ test/TestUnicode.cpp
@@ -0,0 +1,113 @@
+/**********************************************************************************
+ * MIT License
+ * 
+ * Copyright (c) 2018 Antoine Beauchamp
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *********************************************************************************/
+
+#include "TestUnicode.h"
+#include "rapidassist/unicode.h"
+
+namespace ra { namespace unicode { namespace test
+{
+  //--------------------------------------------------------------------------------------------------
+  void TestUnicode::SetUp() {
+  }
+  //--------------------------------------------------------------------------------------------------
+  void TestUnicode::TearDown() {
+  }
+  //--------------------------------------------------------------------------------------------------
+  TEST_F(TestUnicode, testIsAscii)
+  {
+    ASSERT_TRUE ( ra::unicode::IsAscii("foobar") );
+    ASSERT_TRUE ( ra::unicode::IsAscii("\b\t\n\r\\\"!/$%?&*()_+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") );
+
+    ASSERT_FALSE( ra::unicode::IsAscii("école") );   //school in french
+  }
+  //--------------------------------------------------------------------------------------------------
+  TEST_F(TestUnicode, testIsValidCp1252)
+  {
+    ASSERT_TRUE ( IsValidCp1252("foobar") );
+    ASSERT_TRUE ( IsValidCp1252("\\\b\t\n\r\"")); //control characters
+    ASSERT_TRUE ( IsValidCp1252("!/$%?&*()_+"));  //symbols
+    ASSERT_TRUE ( IsValidCp1252("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") );
+    ASSERT_TRUE ( IsValidCp1252("español") );     //spanish
+    ASSERT_TRUE ( IsValidCp1252("école") );       //school in french
+  }
+  //--------------------------------------------------------------------------------------------------
+  TEST_F(TestUnicode, testIsValidIso8859_1)
+  {
+    ASSERT_TRUE ( IsValidIso8859_1("foobar") );
+    ASSERT_TRUE ( IsValidIso8859_1("!/$%?&*()_+"));  //symbols
+    ASSERT_TRUE ( IsValidIso8859_1("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") );
+    ASSERT_TRUE ( IsValidIso8859_1("español") );     //spanish
+    ASSERT_TRUE ( IsValidIso8859_1("école") );       //school in french
+
+    ASSERT_FALSE( IsValidIso8859_1("\\\b\t\n\r\"")); //control characters
+    ASSERT_FALSE( IsValidIso8859_1("\x0d\x0a") );   //CRLF, ISO-8859-1 have no control character
+  }
+  //--------------------------------------------------------------------------------------------------
+  TEST_F(TestUnicode, testIsValidUtf8)
+  {
+    ASSERT_TRUE ( IsValidUtf8("foobar") );
+    ASSERT_TRUE ( IsValidUtf8("\\\b\t\n\r\"")); //control characters
+    ASSERT_TRUE ( IsValidUtf8("!/$%?&*()_+"));  //symbols
+    ASSERT_TRUE ( IsValidUtf8("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") );
+
+    //accent letters from Windows CP1252 encoding
+    ASSERT_TRUE ( IsValidUtf8("espa" "\xC3\xB1" "ol") );  //spanish, U+00F1
+    ASSERT_TRUE ( IsValidUtf8("\xC3\xA9" "cole") );       //school in french
+
+    ASSERT_TRUE ( IsValidUtf8("\x0d\x0a") );    //CRLF
+  }
+  //--------------------------------------------------------------------------------------------------
+#ifdef _WIN32
+  //--------------------------------------------------------------------------------------------------
+  TEST_F(TestUnicode, testAnsiUnicode)
+  {
+    const std::string str_ansi = "école"; //school in french
+
+    std::wstring str_unicode = AnsiToUnicode(str_ansi);
+    std::string str_converted = UnicodeToAnsi(str_unicode);
+
+    ASSERT_EQ( str_ansi.size(), str_unicode.size() );
+    ASSERT_EQ( str_ansi, str_converted );
+  }
+  //--------------------------------------------------------------------------------------------------
+  TEST_F(TestUnicode, testUtf8Unicode)
+  {
+    const std::string str_utf8 = "\303\251cole"; //school in french
+
+    std::wstring str_unicode = Utf8ToUnicode(str_utf8);
+    std::string str_converted = UnicodeToUtf8(str_unicode);
+
+    //The size() method returns the number of character, not the numbre of byte.
+    //The utf8 string have one more character than the unicode version because the first characters is counted as 2 bytes because std::string is expected 1 byte/characters.
+    ASSERT_EQ( str_utf8.size() - 1, str_unicode.size() );  
+    
+    //unicode string is missing a byte because the first utf8 characters is 2 bytes in utf but a single character in unicode.
+    ASSERT_EQ( str_utf8, str_converted );
+  }
+  //--------------------------------------------------------------------------------------------------
+#endif //_WIN32
+  //--------------------------------------------------------------------------------------------------
+} //namespace test
+} //namespace environment
+} //namespace ra
