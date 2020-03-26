58fca12331eedc5972ee7613b0ccf8e3fc46d07e
Antoine <end2endzone@gmail.com>
2020-03-26 09:12:12 -0400

    Updated TestProcessUtf8.testGetCurrentProcessPathUtf8() and TestProcess.testGetCurrentProcessPath() to use CloneProcess().

--- test/TestProcessUtf8.cpp
+++ test/TestProcessUtf8.cpp
@@ -53,76 +53,41 @@ namespace ra { namespace process { namespace test
   }
   //--------------------------------------------------------------------------------------------------
   TEST_F(TestProcessUtf8, testGetCurrentProcessPathUtf8) {
-    std::string log_filename = ra::testing::GetTestQualifiedName() + ".log";
+    static const bool support_utf8 = true;
+    static const std::string separator = ra::filesystem::GetPathSeparatorStr();
 
-    const std::string separator = ra::filesystem::GetPathSeparatorStr();
+    //clone current process executable into another process.
+    std::string test_dir_path;
+    std::string new_process_path;
+    std::string error_message;
+    bool cloned = ra::test::CloneProcess(test_dir_path, new_process_path, support_utf8, error_message);  
+    if (!cloned)
+      FAIL() << error_message;
 
-    //Create a working directory that matches current test name
-    std::string test_dir_name = ra::testing::GetTestQualifiedName();
-    std::string test_dir_path = ra::process::GetCurrentProcessDir() + separator + test_dir_name;
-    bool success = filesystem::CreateDirectory(test_dir_path.c_str());
-    ASSERT_TRUE(success);
-
-    //clone current process executable into another process which name contains an utf8 character.
-    std::string new_process_filename = ra::testing::GetTestQualifiedName() + std::string(".psi_\xCE\xA8_psi");
+    //Run the new executable
+    ra::strings::StringVector arguments;
+    arguments.push_back("--SaveGetCurrentProcessPathUtf8");
 #ifdef _WIN32
-    new_process_filename.append(".exe");
-#endif
-    std::string current_process_path = ra::process::GetCurrentProcessPath();
-    std::string new_process_path_utf8 = test_dir_path + separator + new_process_filename;
-    bool copied = ra::filesystem::CopyFileUtf8(current_process_path, new_process_path_utf8);
-    ASSERT_TRUE(copied);
-
-#ifdef _WIN32
-    //The current RapidAssist does not provide a StartProcessUtf8() api yet.
-    //We cannot launch a process from the command prompt if the process filename have an utf-8 character.
-    //To get around this, we look for the 8.3 filename and use this filename for testing.
-    std::string short_new_process_filename = ra::filesystem::GetShortPathForm(new_process_filename);
-    std::string short_new_process_path = test_dir_path + separator + short_new_process_filename;
-    bool short_full_path_found = ra::filesystem::FileExists(short_new_process_path.c_str());
-    ASSERT_TRUE(short_full_path_found);
-
-    //Run the new process and log the output
-    std::string command;
-    command.append("cd /d \"");
-    command.append(test_dir_path);
-    command.append("\" & ");
-    command.append(short_new_process_filename);
-    command.append(" --OutputGetCurrentProcessPathUtf8");
-    command.append(">");
-    command.append(log_filename);
-    int exit_code = system(command.c_str());
-    ASSERT_EQ(0, exit_code) << "Failed running command: " << command;
+    processid_t pid = StartProcessUtf8(new_process_path, test_dir_path, arguments[0]);
 #elif __linux__
-    //Run the new process and log the output
-    std::string command;
-    command.append("cd \"");
-    command.append(test_dir_path);
-    command.append("\" && chmod 777 ");
-    command.append(new_process_filename);
-    command.append(" && ./");
-    command.append(new_process_filename);
-    command.append(" --OutputGetCurrentProcessPathUtf8");
-    command.append(">");
-    command.append(log_filename);
-    int system_result = system(command.c_str());
-    int exit_code = WEXITSTATUS( system_result );
-    ASSERT_EQ(0, exit_code) << "Failed running command: " << command;
-#endif //_WIN32
+    processid_t pid = StartProcessUtf8(new_process_path, test_dir_path, arguments);
+#endif
+    ASSERT_NE(pid, ra::process::INVALID_PROCESS_ID);
 
-    //Search the log file for a valid utf-8 encoded path
-    std::string log_path = test_dir_path + separator + log_filename;
-    bool log_found = ra::filesystem::FileExists(log_path.c_str());
-    ASSERT_TRUE(log_found);
+    //Search for the generated output file
+    std::string expected_output_file_path = test_dir_path + separator + "SaveGetCurrentProcessPathUtf8.txt";
+    ASSERT_TRUE( ra::filesystem::FileExistsUtf8(expected_output_file_path.c_str()) );
 
-    std::string file_content;
-    bool file_readed = ra::filesystem::ReadTextFile(log_path, file_content);
-    ASSERT_TRUE(file_readed);
+    //Read the file
+    std::string content;
+    bool readed = ra::filesystem::ReadTextFileUtf8(expected_output_file_path, content);
+    ASSERT_TRUE( readed );
 
-    bool found_path = (file_content.find(new_process_path_utf8) != std::string::npos);
-    ASSERT_TRUE(found_path);
+    //ASSERT
+    ASSERT_EQ(new_process_path, content);
 
     //cleanup
+    ra::filesystem::DeleteFileUtf8(expected_output_file_path.c_str());
     ra::filesystem::DeleteDirectoryUtf8(test_dir_path.c_str());
   }
   //--------------------------------------------------------------------------------------------------
